a38371108da8e51dc2f36d73b26e2f2a2303f3ef
Kees van Dieren <keesvandieren@squins.com>
2018-01-29 12:10:36 +0100

    Made code a bit more clear, measure duration

--- src/main/java/com/maqqie/poc/RateCalculationPOI.java
+++ src/main/java/com/maqqie/poc/RateCalculationPOI.java
@@ -1,125 +1,136 @@
 package com.maqqie.poc;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import static org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator.evaluateAllFormulaCells;
 
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+import org.apache.commons.lang3.time.StopWatch;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class RateCalculationPOI {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    public static void main(String[] args) throws IOException {
 
-		
-		File excelFIle = new File(classloader.getResource("Excel1.xlsm").getFile());
+        StopWatch duration = new StopWatch();
+        duration.start();
+        doCalculation();
 
-		XSSFWorkbook excelWorkBook = new XSSFWorkbook(new FileInputStream(excelFIle));
-		FormulaEvaluator formulaEvaluator = excelWorkBook.getCreationHelper().createFormulaEvaluator();
+        duration.stop();
 
-//Caclulating Burtourloon for 100 input values F37
-		for(double input=1.00;input<100;input++){
-			double totalPivotGoalSeek = 0.00;
+        System.out.println("Total duration: " + duration);
+    }
 
-			updateInputCell(excelWorkBook, input);
+    private static void doCalculation() throws IOException {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        XSSFWorkbook excelWorkBook = new XSSFWorkbook(classloader.getResourceAsStream("Excel1.xlsm"));
+        FormulaEvaluator formulaEvaluator = excelWorkBook.getCreationHelper().createFormulaEvaluator();
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        // Calculate bruto uurloon for 100 hourly rates.
+        for (double hourlyRate = 1.00; hourlyRate < 100; hourlyRate++) {
+            double totalPivotGoalSeek;
+
+            setWbInputHourlyRate(excelWorkBook, hourlyRate);
+
+            double testBrutoUurloonValue = hourlyRate; //To bring values closer
+            setWorkbookCalculationBrutoUurloon(excelWorkBook, testBrutoUurloonValue);
+            formulaEvaluator.evaluateAll();
+
+            totalPivotGoalSeek = hourlyRate - (readWbCalculationE8(excelWorkBook) + readWbCalculationF24(excelWorkBook) + readWbCalculationF30(excelWorkBook) + readWbCalculationF35(excelWorkBook));
 
 
-			double f37 = input;//To bring values closer 
-			setF37(excelWorkBook, f37);
-			formulaEvaluator.evaluateAll();
-			totalPivotGoalSeek = input -(readE8(excelWorkBook)+readF24(excelWorkBook)+readF30(excelWorkBook)+readF35(excelWorkBook));
+            StopWatch uurloonCalculationStopwatch = new StopWatch();
+            uurloonCalculationStopwatch.start();
+            while (!((testBrutoUurloonValue - totalPivotGoalSeek < 0.001 && testBrutoUurloonValue - totalPivotGoalSeek > -0.001))) { //limit
+                testBrutoUurloonValue = totalPivotGoalSeek; // reducing the difference between F37 and the pivotValue.
+                setWorkbookCalculationBrutoUurloon(excelWorkBook, testBrutoUurloonValue);
+                evaluateAllFormulaCells(excelWorkBook);
+                totalPivotGoalSeek = hourlyRate -
+                        (readWbCalculationE8(excelWorkBook) +
+                        readWbCalculationF24(excelWorkBook) +
+                        readWbCalculationF30(excelWorkBook) + readWbCalculationF35(excelWorkBook));
+            }
+            uurloonCalculationStopwatch.stop();
 
-			while(!((f37-totalPivotGoalSeek < 0.001 && f37-totalPivotGoalSeek > -0.001))){//limit 
-				f37 = totalPivotGoalSeek;//reducing the difference between F37 and the pivotValue. 
-				setF37(excelWorkBook, f37);
-				XSSFFormulaEvaluator.evaluateAllFormulaCells(excelWorkBook);	
-				totalPivotGoalSeek = input -(readE8(excelWorkBook)+readF24(excelWorkBook)+readF30(excelWorkBook)+readF35(excelWorkBook));
-				
-			}
+            System.out.println("Goal seek achieved; hourlyRate input: " + hourlyRate + " Bruto uurloon calculated: " + df.format(testBrutoUurloonValue) + ", duration: " + uurloonCalculationStopwatch);
+        }
+    }
 
-			System.out.println("Goal seek acheived at :"+f37 +" Input : "+input);
-		}
-		
-	}
+    private static void setWbInputHourlyRate(XSSFWorkbook excelWorkBook, double hourlyRate) {
 
-	public static void updateInputCell(XSSFWorkbook excelWorkBook,double inputValue){
+        XSSFSheet inputSheet = excelWorkBook.getSheetAt(0);
 
-		XSSFSheet inputSheet = excelWorkBook.getSheetAt(0);
+        XSSFRow inputRow = inputSheet.getRow(45);
+        XSSFCell inputCell = inputRow.getCell(2);
+        inputCell.setCellValue(hourlyRate);
 
-		XSSFRow inputRow = inputSheet.getRow(45);
-		XSSFCell inputCell = inputRow.getCell(2);
-		inputCell.setCellValue(inputValue);
+    }
 
-	}
+    private static void setWorkbookCalculationBrutoUurloon(XSSFWorkbook excelWorkBook, double input) {
+        XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
+        XSSFRow brutoUurloonRow = calculationSheet.getRow(36);
+        XSSFCell brutoUUrloonCell = brutoUurloonRow.getCell(5);
+        brutoUUrloonCell.setCellValue(input);
+    }
 
-	public static void setF37(XSSFWorkbook excelWorkBook,double input){
-		XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
-		XSSFRow burtoUrloonRow = calculationSheet.getRow(36);
-		XSSFCell burtoUrloonCell = burtoUrloonRow.getCell(5);
-		burtoUrloonCell.setCellValue(input);
-	}
+    private static double readWbCalculationE8(XSSFWorkbook excelWorkBook) {
 
-	public static double readF37(XSSFWorkbook excelWorkBook){
-		XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
-		XSSFRow burtoUrloonRow = calculationSheet.getRow(36);
-		XSSFCell burtoUrloonCell = burtoUrloonRow.getCell(5);
-		return Double.parseDouble(burtoUrloonCell.getRawValue());
-	}
-	public static double readE8(XSSFWorkbook excelWorkBook){
+        XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
+        XSSFRow E8Row = calculationSheet.getRow(7);
+        XSSFCell E8Cell = E8Row.getCell(4);
 
-		XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
-		XSSFRow E8Row = calculationSheet.getRow(7);
-		XSSFCell E8Cell = E8Row.getCell(4);
+        double E8 = Double.parseDouble(E8Cell.getRawValue());
+        //System.out.println("E8 : "+E8);
+        return (double) Math.round(E8 * 10000) / 10000;
+        //return E8;
+    }
 
-		double E8 = Double.parseDouble(E8Cell.getRawValue());
-		//System.out.println("E8 : "+E8);
-		return (double) Math.round(E8 * 10000) / 10000;
-		//return E8;
-	}
+    private static double readWbCalculationF24(XSSFWorkbook excelWorkBook) {
 
-	public static double readF24(XSSFWorkbook excelWorkBook){
+        XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
+        XSSFRow F24Row = calculationSheet.getRow(23);
+        XSSFCell F24Cell = F24Row.getCell(5);
 
-		XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
-		XSSFRow F24Row = calculationSheet.getRow(23);
-		XSSFCell F24Cell = F24Row.getCell(5);
+        double F24 = Double.parseDouble(F24Cell.getRawValue());
+        //System.out.println("F24 : "+F24);
+        return (double) Math.round(F24 * 10000) / 10000;
 
-		double F24 = Double.parseDouble(F24Cell.getRawValue());
-		//System.out.println("F24 : "+F24);
-		return (double) Math.round(F24 * 10000) / 10000;
+        //return F24;
+    }
 
-		//return F24;
-	}
+    private static double readWbCalculationF30(XSSFWorkbook excelWorkBook) {
 
-	public static double readF30(XSSFWorkbook excelWorkBook){
+        XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
+        XSSFRow F30Row = calculationSheet.getRow(29);
+        XSSFCell F30Cell = F30Row.getCell(5);
 
-		XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
-		XSSFRow F30Row = calculationSheet.getRow(29);
-		XSSFCell F30Cell = F30Row.getCell(5);
+        double F30 = Double.parseDouble(F30Cell.getRawValue());
+        //System.out.println("F30 : "+F30);
+        return (double) Math.round(F30 * 10000) / 10000;
 
-		double F30 = Double.parseDouble(F30Cell.getRawValue());
-		//System.out.println("F30 : "+F30);
-		return (double) Math.round(F30 * 10000) / 10000;
+        //return F30;
+    }
 
-		//return F30;
-	}
+    private static double readWbCalculationF35(XSSFWorkbook excelWorkBook) {
 
-	public static double readF35(XSSFWorkbook excelWorkBook){
+        XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
+        XSSFRow F35Row = calculationSheet.getRow(34);
+        XSSFCell F35Cell = F35Row.getCell(5);
 
-		XSSFSheet calculationSheet = excelWorkBook.getSheetAt(2);
-		XSSFRow F35Row = calculationSheet.getRow(34);
-		XSSFCell F35Cell = F35Row.getCell(5);
+        double F35 = Double.parseDouble(F35Cell.getRawValue());
+        //System.out.println("F35 : "+F35);
+        return (double) Math.round(F35 * 10000) / 10000;
 
-		double F35 = Double.parseDouble(F35Cell.getRawValue());
-		//System.out.println("F35 : "+F35);
-		return (double) Math.round(F35 * 10000) / 10000;
-
-		//return F35;
-	}
+        //return F35;
+    }
 
 }
